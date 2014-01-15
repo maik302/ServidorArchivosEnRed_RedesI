660a97de7af440cd7025a7edb21582c89ea6c804
mwoo <maik302@gmail.com>
2014-01-15 17:22:31 -0430

    Commit antes de irse a casa. 5:30pm 15/01
    -Maik

--- cliente/cliente_rmifs.java
+++ cliente/cliente_rmifs.java
@@ -7,7 +7,7 @@ import java.rmi.NotBoundException;
 
 public class cliente_rmifs {
   private static String servidor;
-  private static String puerto = "210912";
+  private static String puerto = "20912";
   private static validador_usuario usuario;
 
   private static void interpretar_argumentos(String[] args) {
@@ -16,13 +16,14 @@ public class cliente_rmifs {
     try {
       i = 0;
       if(args[0].equals("-f")) {
-        usuario = obtener_usuario_archivo(new File(args[1]));
+        usuario = funciones_cliente.obtener_usuario_archivo(new File(args[1]));
+        i = 2;
       }
       else {
-        i = 2;
-        usuario = obtener_usuario_teclado();
+        usuario = funciones_cliente.obtener_usuario_teclado();
         if(args[i].equals("-m")) {
           servidor = args[i+1];
+          System.out.println(args[i+1]);
           if(args[i+2].equals("-p")) {
             puerto = args[i+3];
           }
@@ -32,9 +33,9 @@ public class cliente_rmifs {
           //Java no evalua toda la expresion, con que el primero se haga false,
           //no sigue evaluando (evaluacion perezosa, quizas?)
           if(args.length > i+4 && args[i+4].equals("-c")) {
-            interpretar_comandos_archivo(new File(args[i+5]));
+            funciones_cliente.interpretar_comandos_archivo(new File(args[i+5]));
           }
-          interpretar_comandos_teclado();
+          funciones_cliente.interpretar_comandos_teclado();
           
         }
         else {
@@ -47,25 +48,26 @@ public class cliente_rmifs {
       System.exit(0);
     }
     catch(ArgumentosException e) {
-      System.out.println("Error en la especificacion de llamada del programa.");
+      System.out.println("error en la especificacion de llamada del programa.");
       System.exit(0);
     }
-    catch(FileNotFoundException e) {
-      System.out.println("Archivo no encontrado.");
-      System.exit(0);
-    }
+    //catch(FileNotFoundException e) {
+    //  System.out.println("Archivo no encontrado.");
+    //  System.exit(0);
+    //}
 
   }
   
   public static void main(String[] args) {
     try {
+      interpretar_argumentos(args);
       a_rmifs_interfaz a_usuario = (a_rmifs_interfaz)
         //Esta opcion comentada debe ser probada en la Universidad, 
         //por la lejania del servidor tarda en responder.
         //Naming.lookup("rmi://irulan.ldc.usb.ve:21000/a_rmifs_Service");
         Naming.lookup("rmi://"+servidor+":"+puerto+"/a_rmifs_Service");
-      interpretar_argumentos(args);
       //PRUEBAS
+      a_usuario.validar("maria","123");
 
     }
     catch (MalformedURLException murle) {
